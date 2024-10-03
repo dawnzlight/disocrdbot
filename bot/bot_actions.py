2aef99da95e5c7829d04d382e7007541657a2916
CoCo9122 <banaji.yu0517@outlook.jp>
2024-10-04 00:07:48 +0900

    pull requestをwebhookにも反応させる

--- bot/bot_actions.py
+++ bot/bot_actions.py
@@ -25,7 +25,7 @@ async def voice_channel_in_and_out_send(member, before, after, client, config):
         elif before.channel is not None:
             await text_channel_in_and_out_send.send(f'{member.name} が {before.channel.name} から退室したよ。')
 
-def pull_request_summary_action(github, gemini):
+def pull_requests_summary_action(github, gemini):
     """
     プルリクエストの要約を行う関数
     :param
@@ -42,8 +42,8 @@ def pull_request_summary_action(github, gemini):
 
     for i, pull_request in enumerate(pull_requests):
 
-        reviewers = github.assign_reviewer('all-you-can-drink', pull_request['number'], pull_request['user']).json()
-        reviewers = [github.get_disocrd_member_id(reviewer['login']) for reviewer in reviewers['requested_reviewers']][0]
+        # reviewers = github.assign_reviewer('all-you-can-drink', pull_request['number'], pull_request['user']).json()
+        # reviewers = [github.get_disocrd_member_id(reviewer['login']) for reviewer in reviewers['requested_reviewers']][0]
 
         response = github.get_diff('all-you-can-drink', pull_request['number']).text
         changed_files = extract_changed_files_from_diff(response)
@@ -57,19 +57,50 @@ def pull_request_summary_action(github, gemini):
 
         message = response['candidates'][0]['content']['parts'][0]['text']
 
-        try:
-            github.comment_to_pull_request('all-you-can-drink', pull_request['number'], message)
-        except Exception as e:
-            print(e)
+        # try:
+        #     github.comment_to_pull_request('all-you-can-drink', pull_request['number'], message)
+        # except Exception as e:
+        #     print(e)
 
-        if len(reviewers) == 0:
+        if len(pull_request['reviewers']) == 0:
             message = message + f"\nURL：{pull_request['html_url']}"
         else:
-            message = message + f"\n<@{reviewers}>さん。レビューお願いね！！。" + \
+            message = message + f"\n<@{pull_request['reviewers'][0]}>さん。レビューお願いね！！。" + \
             f"\nURL：{pull_request['html_url']}"
             
         messages.append(message)
 
     return messages
 
+def pull_request_summary_action(github, gemini, pull_request_number):
+
+    pull_request = github.get_pull_request('all-you-can-drink', pull_request_number)
+
+    reviewers = github.assign_reviewer('all-you-can-drink', pull_request['number'], pull_request['user']).json()
+    reviewers = [github.get_disocrd_member_id(reviewer['login']) for reviewer in reviewers['requested_reviewers']][0]
+
+    response = github.get_diff('all-you-can-drink', pull_request['number']).text
+    changed_files = extract_changed_files_from_diff(response)
+
+    prompt = 'プルリクエストの変更ファイルを見て、その内容について要約してください。' + \
+        f"5行程度にまとめてください。また回答は「{pull_request['user']}さんからのプルリクだよ。」で始めてください。" \
+        '変更があったファイルは以下の通りです。元気よくお願いします！！' + \
+        ' '.join([f"{file['file_name']}、追加された行：{file['diff']['plus']}、削除された行：{file['diff']['minus']}" for file in changed_files])
     
+    response = gemini.generate_content(prompt=prompt)
+
+    message = response['candidates'][0]['content']['parts'][0]['text']
+
+    try:
+        github.comment_to_pull_request('all-you-can-drink', pull_request['number'], message)
+    except Exception as e:
+        print(e)
+
+    if len(pull_request['reviewers']) == 0:
+        message = message + f"\nURL：{pull_request['html_url']}"
+    else:
+        message = message + f"\n<@{pull_request['reviewers'][0]}>さん。レビューお願いね！！。" + \
+        f"\nURL：{pull_request['html_url']}"
+
+    return message
+
