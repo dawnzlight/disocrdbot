926db3cee80059254955ff2c4650fc01ebcf8f53
CoCo9122 <banaji.yu0517@outlook.jp>
2024-10-03 22:11:16 +0900

    github pull requestをできるだけ自動化

--- bot/github_api.py
+++ bot/github_api.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -17,21 +19,29 @@ class Github:
             'CoCo9122': '691214730643374080',
             'Maneo9910': '691094528349831259'
         }
+        self.reviewer = ['CoCo9122', 'Maneo9910']
+
+    def get_disocrd_member_id(self, member):
+        if member in self.discord_member_id:
+            return self.discord_member_id[member]
+        else:
+            return member
 
     def get_pull_requests(self, repo):
 
         headers = self.headers
         headers['Accept'] = 'application/vnd.github.v3+json'
 
-        responses = requests.get(f"{self.url}/repos/{self.owner}/{repo}/pulls", headers=self.headers).json()
+        responses = requests.get(f"{self.url}/repos/{self.owner}/{repo}/pulls", headers=headers).json()
 
         pull_requests = [ {
             'url': response['url'],
+            'html_url': response['html_url'],
             'title': response['title'],
             'number': response['number'],
             'user':response['user']['login'],
             "created_at": response['created_at'],
-            "reviewers": [ self.discord_member_id[reviewer['login']] for reviewer in response['requested_reviewers']],
+            "reviewers": [ self.get_disocrd_member_id(reviewer['login']) for reviewer in response['requested_reviewers']],
         } for response in responses]
 
         return pull_requests
@@ -39,11 +49,24 @@ class Github:
     def get_diff(self, repo, number):
         headers = self.headers
         headers['Accept'] = 'application/vnd.github.v3.diff'
-        return requests.get(f"{self.url}/repos/{self.owner}/{repo}/pulls/{number}", headers=self.headers)
+        return requests.get(f"{self.url}/repos/{self.owner}/{repo}/pulls/{number}", headers=headers)
     
     def comment_to_pull_request(self, repo, number, body):
-
+        headers = self.headers
+        headers['Accept'] = 'application/vnd.github.v3+json'
         data = {
             'body': body
         }
-        requests.post(f"{self.url}/repos/{self.owner}/{repo}/issues/{number}/comments", headers=self.headers, data=data)
+        return requests.post(f"{self.url}/repos/{self.owner}/{repo}/issues/{number}/comments", headers=self.headers, data=json.dumps(data))
+
+    def assign_reviewer(self, repo, number, user):
+        headers = self.headers
+        headers['Accept'] = 'application/vnd.github.v3+json'
+        data = {
+            'reviewers': self.reviewer.copy()
+        }
+        if user:
+            data['reviewers'].remove(user)
+            return requests.post(f"{self.url}/repos/{self.owner}/{repo}/pulls/{number}/requested_reviewers", headers=headers, data=json.dumps(data))
+        else:
+            return [{'requested_reviewers': []}]
